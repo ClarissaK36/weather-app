5dffbdf706748be42da8fbd8fd3097cb589f5086
ClarissaK36 <clarissak.official@gmail.com>
2024-10-05 16:49:24 +0200

    Updated pvt API Key

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function displayTemperature(response) {
     let searchInputElement = document.querySelector("#search-input");
     let city = searchInputElement.value;
   
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiKey = "16dte5fe6bd880b3859a456a73aoea0f";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   
     axios.get(apiUrl).then(displayTemperature);
